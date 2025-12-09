c4f40dea9fd257dccc862539d944e0b5a03d8858
Bageerathan <bageesaravanan@gmail.com>
2025-12-08 19:00:58 -0600

    added preliminary events page; added and fleshed out logic behind dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { Routes, Route } from 'react-router-dom';
 import './css/app.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import DarkModeToggle from './components/DarkModeToggle';
 import Home from './pages/Home';
 import ResourceDirectoryPage from './pages/ResourceDirectoryPage';
+import Events from './pages/Events';
 import Contact from './pages/Contact';
 import Highlights from './pages/Highlights';
 import SubmitResource from './pages/SubmitResource';
@@ -12,10 +14,12 @@ import SubmitResource from './pages/SubmitResource';
 export default function App() {
   return (
     <>
+      <DarkModeToggle />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resources" element={<ResourceDirectoryPage />} />
+        <Route path="/events" element={<Events />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/highlights" element={<Highlights />} />
         <Route path="/submit-resource" element={<SubmitResource />} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { DarkModeProvider } from './context/DarkModeContext'
 import './css/index.css'
+import './css/darkMode.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <DarkModeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DarkModeProvider>
   </StrictMode>
 )
